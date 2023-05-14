745dc25e4ee3fd53398fed2465ef8e62f026deb7
Samira Kabir Rima <98251105+Samirarim@users.noreply.github.com>
2023-05-14 09:42:00 +0600

    deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,34 @@ import { CookModule } from './Employee/cook/cook.module';
 import { DMModule } from './Employee/deliveryman/deliveryman.module';
 import { ManagerModule } from './Employee/manager/manager.module';
 import { WaiterModule } from './Employee/waiter/waiter.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [ManagerModule, WaiterModule, DMModule, CookModule, TypeOrmModule.forRoot({
-    type:'postgres',
-    host: 'localhost',
-    port: 5432,
+    // type:'postgres',
+    // host: 'localhost',
+    // port: 5432,
+    // username: 'postgres',
+    // password: 'hello',
+    // database: 'RMS',
+    // autoLoadEntities: true,
+    // synchronize: true,})],
+    type: 'postgres',
+    host: 'containers-us-west-96.railway.app',
+    port: 6448,
     username: 'postgres',
-    password: 'hello',
-    database: 'RMS',
+    password: 'Ifx7UMo8egMuYjodq93y',
+    database: 'railway',
     autoLoadEntities: true,
-    synchronize: true,})],
+    synchronize: true,
+  }
+  ),
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', '../public'), // added ../ to get one folder back
+    serveRoot: '/public/' //last slash was important
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
